499cd3d4e7d90fdb5d7fa696903fc6fe83c2386c
Yeonho Jang <siabard@gmail.com>
2023-03-01 22:44:06 +0900

    entity에서 속성 컴포넌트로 추출

--- engine/component.lisp
+++ engine/component.lisp
@@ -1,10 +1,17 @@
 (in-package #:dino-lisp)
 
-(defstruct movable speed x y)
+(defstruct movable speed max-speed x y dx dy new-x new-y)
 
-(defstruct animatable span animation)
+(defstruct animatable
+  animation-span
+  animation-map
+  current-animation
+  current-frame
+  elapsed-time )
 
 (defstruct collidable rect channel)
+
+(defstruct renderable texture width height atlas)
 
 ;;; typep를 이용해서 타입의 동일성을 검증할 수 있다.
 ;;; (typep (make-movable) 'movable) -> T
